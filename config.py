7ae60e0567ec57fd4e7635da67c110744185c965
Bear Golightly <bear@nashvillewraps.com>
2019-12-14 18:04:24 -0600

    end of chapter 4

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = "I am so moist"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db?check_same_thread=False')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
